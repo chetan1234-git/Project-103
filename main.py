f17bcadc9305b5f7d6306cc852123b61e289f30a
chetan1234-git <71207837+chetan1234-git@users.noreply.github.com>
2022-01-30 10:27:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('Copy+of+data+-+data.csv')
+
+fig = px.scatter(df, x ='date', y='cases', color='country', title='COVID-19 CASES')
+
+fig.show()
